4a395b34104c7d1ad96eb6ac06dc6bab303ed993
Ryan Peabody <rpeabody43@gmail.com>
2023-11-29 19:58:07 -0500

    Create logo

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"20hh/engine"
+	"strings"
+
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const asciiArt = `
+              $$$
+         $$$$$$$$$$$$$
+     $$$$$$$$$$$$$$$$$$$$$
+  $$$$$$$$$       $$$$$$$$$$$    /$$$$$$   /$$$$$$  /$$   /$$ /$$   /$$
+  $$$$$$               $$$$$$   /$$__  $$ /$$$_  $$| $$  | $$| $$  | $$
+  $$$$$$        $      $$$$$$  |__/  \ $$| $$$$\ $$| $$  | $$| $$  | $$
+  $$$$       $$$        $$$$$    /$$$$$$/| $$ $$ $$| $$$$$$$$| $$$$$$$$
+  $$$$$   $$$$$         $$$$$   /$$____/ | $$\ $$$$| $$__  $$| $$__  $$
+  $$$$$$$$$$$$           $$$$  | $$      | $$ \ $$$| $$  | $$| $$  | $$
+  $$$$$$$$$$             $$$$  | $$$$$$$$|  $$$$$$/| $$  | $$| $$  | $$
+     $$$$$$$$$$$$$$$$$$$$$     |________/ \______/ |__/  |__/|__/  |__/
+         $$$$$$$$$$$$$
+              $$$
+`
+
+func printBanner() {
+	fmt.Print(asciiArt)
+	fmt.Println()
+	fmt.Println("Enter 'uci' to start or 'quit' to exit")
+	fmt.Println()
+}
+
+func main() {
+	printBanner()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		command, _ := reader.ReadString('\n')
+		command = strings.Fields(command)[0]
+		switch command {
+		case "uci":
+			engine.UCILoop()
+			return
+		case "quit":
+			return
+		default:
+			fmt.Printf("\"%s\" not recognized\n", command)
+		}
+	}
+}
